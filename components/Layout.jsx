0188b68867f4102f2126b2f3934ed2e093876b52
Daniel Mason <danmasson0@gmail.com>
2022-05-20 23:14:51 -0400

    finish setting up property listings for home, and created basic layout using Chakra.ui

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,25 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 
-const Layout = () => {
-    <>
-        <Head>
-            <title>REal Estate</title>
-        </Head>
-    </>
+const Layout = ({ children }) => {
+    return (
+        <>
+            <Head>
+                <title>Real Estate</title>
+            </Head>
+            <Box maxWidth="1280px" m="auto">
+                <header>
+                    NavBar
+                </header>
+                <main>
+                    {children}
+                </main>
+                <footer>
+                    Footer
+                </footer>
+            </Box>
+        </>
+    );
 }
+
+export default Layout;
