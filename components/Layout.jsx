f4e667e5d9d075020d4b0484ba423802ebc6b382
Daniel Mason <danmasson0@gmail.com>
2022-05-29 09:06:46 -0400

    created functional components for navbar and footer, incorporated them into reality app

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
 const Layout = ({ children }) => {
     return (
         <>
@@ -9,13 +12,13 @@ const Layout = ({ children }) => {
             </Head>
             <Box maxWidth="1280px" m="auto">
                 <header>
-                    NavBar
+                    <NavBar />
                 </header>
                 <main>
                     {children}
                 </main>
                 <footer>
-                    Footer
+                    <Footer />
                 </footer>
             </Box>
         </>
